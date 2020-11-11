85e6e7227b853bd5339b034c3bf81a6e9140aa1d
Alex <1414818093@qq.com>
2020-11-11 17:47:05 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -36,3 +36,12 @@ echo 'execute command  ===> cd ../ && rm -rf ./temp_deploy'
 #cd ../ && rm -rf ./temp_deploy
 
 echo "Congratulations ! The end !"
+
+echo -e "\033[30m 黑色字 \033[0m"
+echo -e "\033[31m 红色字 \033[0m"
+echo -e "\033[32m 绿色字 \033[0m" 
+echo -e "\033[33m 黄色字 \033[0m" 
+echo -e "\033[34m 蓝色字 \033[0m" 
+echo -e "\033[35m 紫色字 \033[0m" 
+echo -e "\033[36m 天蓝字 \033[0m" 
+echo -e "\033[37m 白色字 \033[0m"
