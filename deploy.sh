c396ef293d6b63007f5a9459fab482b7dd8576b3
pudongping <276558492@qq.com>
2021-06-08 12:14:27 +0800

    重新优化自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -24,18 +24,9 @@ function yellow(){
 }
 
 # 定义 github 仓库地址，和存放静态页面的分支，使用前请修改这两个变量的值
-case $1 in
-'github')
-  my_repo='git@github.com:pudongping/notes.git'
-  ;;
-'gitee')
-  my_repo='git@gitee.com:pudongping/notes.git'
-  ;;
-  *)
-  orangered "please type 'github' or 'gitee' to deploy"
-  ;;
-esac
-
+my_repo_1='git@gitee.com:pudongping/notes.git'
+# 如果你有多个远程仓库，请自行添加下面这一行
+my_repo_2='git@github.com:pudongping/notes.git'
 my_branch='gh-pages'
 
 # 当前发布时间
@@ -50,7 +41,7 @@ readonly my_repo
 readonly my_branch
 
 # 判断是否存在 gitbook 命令
-if [ ! -f "$gitbook_path" ];then
+if [ ! -f $gitbook_path ];then
     orangered 'Sorry ! The gitbook path is not found ! Please check if the gitbook has been installed .'
     exit 0
 fi
@@ -59,36 +50,40 @@ fi
 blue 'Deploy starting ......'
 
 # 生成静态文件
-orangered 'execute command  ===> gitbook build .'
+orangered 'Generate static files'
 gitbook build .
 
-# 复制已经打包好的静态文件夹 _book 到临时文件夹 $temp_deploy 且进入 $temp_deploy 文件夹内
-orangered "execute command  ===> cp -r ./_book ./${temp_deploy} && cd ./${temp_deploy}"
-cp -r ./_book ./$temp_deploy && cd ./$temp_deploy
+# 如果不存在发布目录
+if [ ! -d ./$temp_deploy ]
+then
+  # 第一次执行部署命令时
+  cp -r ./_book ./$temp_deploy && cd ./$temp_deploy
+  # 初始化一个仓库，仅仅是做了一个初始化的操作，项目里的文件还没有被跟踪
+  git init
+  orangered 'execute command  ===> git init'
+  # 添加远程仓库地址
+  git remote add origin $my_repo_1
+  # 如果你有多个远程仓库，请自行添加下面这一行
+  git remote set-url --add origin $my_repo_2
+  git add -A
+  git commit -m "First commit"
+else
+  # 多次执行部署命令时
+  cd ./$temp_deploy
+  rm -rf *
+  cp -r ../_book/* ./
+  # 添加所有的修改到 git 的缓存区
+  git add -A
+  git commit -m "Site updated: ${current_time}"
+fi
 
-# 初始化一个仓库，仅仅是做了一个初始化的操作，项目里的文件还没有被跟踪
-orangered 'execute command  ===> git init'
-git init
-
-# 添加远程仓库地址
-orangered "execute command  ===> git remote add origin ${my_repo}"
-git remote add origin $my_repo
-
-# 添加所有的修改到 git 的缓存区
-orangered 'execute command  ===> git add -A'
-git add -A
-
-# 将缓存区的内容提交到本地仓库中，并新建分支和切换到新分支
-orangered "execute command  ===> git commit -m 'Site updated: ${current_time}' && git checkout -b ${my_branch}"
-git commit -m "Site updated: ${current_time}" && git checkout -b $my_branch
+# 新建分支和切换到新分支。这里一定要切换到发布代码的分支上，不然可能会出现意想不到的结果！
+git checkout -b $my_branch
 
 # 发布到远程仓库 gh-pages 分支，因为这里都是使用 gitbook 编译后的文件，因此可以直接通过 -f 参数覆盖掉远程分支，文件要是丢失了的话，多编译几次，再多强制推送
 orangered "execute command  ===> git push -f -u origin ${my_branch}"
 git push -f -u origin $my_branch
 
-# 返回上层目录，并删除临时目录
-# ====> 发布完毕之后一定要删除发布代码的目录，不然每次发布的时候，都会将发布代码的目录编译进去，多次发布后，将会使得代码仓库成指数级别增长，造成仓库巨大
-orangered "execute command  ===> cd ../ && rm -rf ./$temp_deploy"
-cd ../ && rm -rf ./$temp_deploy
+green "Congratulations, deploy successful! The end !"
 
-green "Congratulations ! The end !"
+exit 0
